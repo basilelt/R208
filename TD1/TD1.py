4ec7652eb68011c9dbd1a8edfff799d1397cac3e
basilelt <lethiecb@protonmail.com>
2023-01-18 18:21:51 +0100

    TD1

--- TD1/TD1.py
+++ TD1/TD1.py
@@ -12,26 +12,37 @@ def get_bw(file): ## Bande passante
 
 def get_cr(file): ## Bits redondants
     lst_file = file.split("-")
+    lst_file[3] = lst_file[3].replace(".csv", "")
     return lst_file[3]
 
-def load_data(path_bw_sf, path_raw, file):
-    df = pandas.read_csv(os.path.join(path_raw, file), sep = ",", delimiter = None)
-    df.columns = ["Time(S)", "Current(A)", "Voltage(V)", "Energy(J)", "UART"]
-
-    df["UART"] = df["UART"].astype("string")
-
-    df["CR"] = get_cr(file)
-
-    df.to_csv(os.path.join(path_bw_sf, f"dataset-{get_bw(file)}-{get_sf(file)}.csv"))
-
+def load_data(path, file):
+    df = pandas.read_csv(os.path.join(path, file), sep = ",", delimiter = None, dtype = {"File UART (TXT)" : str})
+    return df
 
 
 path = os.path.normpath(__file__.replace("TD1.py", ""))
 path_raw = os.path.join(path, "raw")
 path_bw_sf = os.path.join(path, "bw-sf")
+path_bw = os.path.join(path, "bw")
 
 for i in os.listdir(path_raw):
-    load_data(path_bw_sf, path_raw, i)
+    df = load_data(path_raw, i)
+    df.columns = ["Time(S)", "Current(A)", "Voltage(V)", "Energy(J)", "UART"]
+
+    df["CR"] = get_cr(i)
+
+    df.to_csv(os.path.join(path_bw_sf, f"dataset-{get_bw(i)}-{get_sf(i)}-{get_cr(i)}.csv"), index = False)
+
+df_7 = pandas.DataFrame(columns = ["Time(S)", "Current(A)", "Voltage(V)", "Energy(J)", "UART", "CR"])
+df_12 = pandas.DataFrame(columns = ["Time(S)", "Current(A)", "Voltage(V)", "Energy(J)", "UART", "CR"])
 
 for i in os.listdir(path_bw_sf):
-    
+    sf = get_sf(i)
+    if get_sf(i) == "7":
+        df_7 = pandas.concat([df_7, load_data(path_bw_sf, i)])
+
+    elif get_sf(i) == "12":
+        df_12 = pandas.concat([df_12, load_data(path_bw_sf, i)])
+
+df_7.to_csv(os.path.join(path_bw, f"dataset-7.csv"), index = False)
+df_12.to_csv(os.path.join(path_bw, f"dataset-12.csv"), index = False)
